aba5c5b1d2c1556b4e03d24037948afd415575bf
Joao Erick <jerick1415@outlook.com>
2020-06-09 16:55:32 -0300

    Definindo aleatoriedade da comida e o tamanho da cobrinha

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function criarBG(){
 function criarCobrinha(){
     for(i=0; i<snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[0].x, snake[0].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
@@ -59,7 +59,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
